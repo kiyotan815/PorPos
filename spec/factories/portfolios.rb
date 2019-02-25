9bddad4b36ce1c5718f2d19ac04398f57979e5eb
kiyotan815 <kyt.ibaragi0815@gmail.com>
2019-02-25 22:28:40 +0900

    add scaffold portfolio

---
+++ spec/factories/portfolios.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :portfolio do
+    title { "MyString" }
+    description { "MyText" }
+    url { "MyText" }
+    repository { "MyText" }
+    references { "" }
+  end
+end
