5fd6bc1149eeb6d16a70a1581ef5873550ffa6d3
kiyotan815 <kyt.ibaragi0815@gmail.com>
2019-02-25 22:24:43 +0900

    create portfolios model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_084240) do
+ActiveRecord::Schema.define(version: 2019_02_25_132115) do
+
+  create_table "portfolios", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.text "url"
+    t.text "repository"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_portfolios_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_portfolios_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
