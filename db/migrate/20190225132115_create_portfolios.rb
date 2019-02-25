be0cb0ea2061f5ac0f8f222d31be9a7b03619134
kiyotan815 <kyt.ibaragi0815@gmail.com>
2019-02-25 22:22:58 +0900

    add migration portfolio

---
+++ db/migrate/20190225132115_create_portfolios.rb
@@ -0,0 +1,14 @@
+class CreatePortfolios < ActiveRecord::Migration[5.2]
+  def change
+    create_table :portfolios do |t|
+      t.string :title
+      t.text :description
+      t.text :url
+      t.text :repository
+      t.user :references
+
+      t.timestamps
+    end
+    add_index :portfolios, [:user_id, :created_at]
+  end
+end
