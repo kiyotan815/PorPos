dccc0097dbdaba364056837f1a11d9082ea3a598
kiyotan815 <kyt.ibaragi0815@gmail.com>
2019-02-25 22:29:34 +0900

    association add

--- spec/factories/portfolios.rb
+++ spec/factories/portfolios.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: portfolios
+#
+#  id          :bigint(8)        not null, primary key
+#  title       :string(255)
+#  description :text(65535)
+#  url         :text(65535)
+#  repository  :text(65535)
+#  user_id     :bigint(8)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 FactoryBot.define do
   factory :portfolio do
     title { "MyString" }
