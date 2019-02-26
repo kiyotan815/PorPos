75fd66dba0d75986a7b46c88057bcf552c020fb1
kiyotan815 <kyt.ibaragi0815@gmail.com>
2019-02-26 09:45:36 +0900

    portfolios view fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@
 #
 
 class User < ApplicationRecord
-  has_many :portfolios
+  has_many :portfolios, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
